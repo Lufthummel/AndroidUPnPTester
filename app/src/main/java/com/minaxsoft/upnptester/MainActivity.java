a1db231f60b89aad72b0f493eceaece3bdb6d7eb
Lufthummel <holger.kremmin@gmail.com>
2018-01-29 18:27:10 +0100

    working draft

--- app/src/main/java/com/minaxsoft/upnptester/MainActivity.java
+++ app/src/main/java/com/minaxsoft/upnptester/MainActivity.java
@@ -1,13 +1,64 @@
 package com.minaxsoft.upnptester;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        log = (TextView)findViewById(R.id.log);
+        log.setMovementMethod(new ScrollingMovementMethod());
+        log.setText("");
+
+        Button discover = (Button) findViewById(R.id.discover) ;
+        discover.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        startDiscovery();
+                    }
+                });
+
+            }
+        });
+
+        Button clear = (Button) findViewById(R.id.clear) ;
+        clear.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                log.setText("");
+            }
+        });
+    }
+
+    protected void startDiscovery() {
+
+
+        UPnPDiscovery discover = new UPnPDiscovery(this.getApplicationContext(), log);
+        discover.execute();
+        try {
+            Thread.sleep(1500);
+            String[] devices = discover.addresses.toArray(new String[discover.addresses.size()]);
+
+            for (String device : devices) {
+                log.append(device);
+            }
+
+        } catch (InterruptedException e) {
+            log.append("Exception during discovery \n");
+        }
+
     }
 }
---
+++ app/src/main/java/com/minaxsoft/upnptester/UPnPDiscovery.java
@@ -0,0 +1,115 @@
+package com.minaxsoft.upnptester;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashSet;
+
+/**
+ * @author Bernd Verst(@berndverst)
+ */
+public class UPnPDiscovery extends AsyncTask
+{
+    HashSet<String> addresses = new HashSet<>();
+    Context ctx;
+    TextView log;
+
+
+
+    public UPnPDiscovery(Context context, TextView tv) {
+        ctx = context;
+        log = tv;
+    }
+
+    protected void logger(String _msg) {
+
+        final String msg = _msg;
+        log.post(new Runnable() {
+            public void run() {
+
+                log.append(msg);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+
+        WifiManager wifi = (WifiManager)ctx.getSystemService( ctx.getApplicationContext().WIFI_SERVICE );
+
+        if(wifi != null) {
+            logger("Got Wifi Manager \n");
+
+            WifiManager.MulticastLock lock = wifi.createMulticastLock("The Lock");
+            lock.acquire();
+            logger("Multistate Lock acquired  \n");
+            DatagramSocket socket = null;
+
+            try {
+
+                InetAddress group = InetAddress.getByName("239.255.255.250");
+                int port = 1900;
+                String query =
+                        "M-SEARCH * HTTP/1.1\r\n" +
+                                "HOST: 239.255.255.250:1900\r\n"+
+                                "MAN: \"ssdp:discover\"\r\n"+
+                                "MX: 1\r\n"+
+                                //"ST: urn:schemas-upnp-org:service:AVTransport:1\r\n"+  // Use for Sonos
+                                "ST: ssdp:all\r\n"+  // Use this for all UPnP Devices
+                                "\r\n";
+
+                socket = new DatagramSocket(port);
+                socket.setReuseAddress(true);
+
+                DatagramPacket dgram = new DatagramPacket(query.getBytes(), query.length(),
+                        group, port);
+                socket.send(dgram);
+
+                logger("sending discovery " + query + "\n");
+
+                long time = System.currentTimeMillis();
+                long curTime = System.currentTimeMillis();
+
+                // Let's consider all the responses we can get in 1 second
+                while (curTime - time < 1000) {
+                    DatagramPacket p = new DatagramPacket(new byte[1024], 1024);
+                    socket.receive(p);
+
+                    String s = new String(p.getData(), 0, p.getLength());
+
+                    logger("Answer received" + s +  "\n");
+                    /*if (s.toUpperCase().equals("HTTP/1.1 200")) {
+                        addresses.add(p.getAddress().getHostAddress());
+                    }
+                    */
+
+
+                    curTime = System.currentTimeMillis();
+                }
+
+            } catch (Exception e) {
+                logger("Multistate Lock acquired  \n" + e.toString());
+            }
+            finally {
+
+                socket.close();
+                logger("Socket closed  \n");
+            }
+            lock.release();
+            logger("Lock released  \n");
+        }
+        return null;
+    }
+
+
+}
